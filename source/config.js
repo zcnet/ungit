5627bf238b4e60be8288cd2c23e3ecddd2b1744c
b.Schreiber <b.schreiber@trisinus.de>
2014-04-02 10:06:47 +0200

    Newline

--- source/config.js
+++ source/config.js
@@ -66,6 +66,7 @@ var defaultConfig = {
 
   // Restrict to Directory
   restrictToDirectory: undefined,
+
   // Specify a custom command to launch. `%U` will be replaced with the URL
   //  that corresponds with the working git directory.
   //
